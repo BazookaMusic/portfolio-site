cc5499ce345af53ca242cbc172df6a960de5fe4f
BazookaMusic <sotirisdragonas@gmail.com>
2022-02-18 19:30:59 +0200

    typo

--- pages/projects.js
+++ pages/projects.js
@@ -13,8 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            My creations
-          </p>
+            My creations.</p>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
